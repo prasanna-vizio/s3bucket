757d8dd56a5e800b4f538e58379955a1d05a6fea
prasanna-vizio <167919134+prasanna-vizio@users.noreply.github.com>
2024-11-26 01:50:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,10 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
 
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
